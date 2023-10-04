a9d215f3b72320eb86aaa7ea645a70a18e9f9d43
r2d209git <r2d209@gmail.com>
2023-10-04 13:14:44 +0900

    gpu 사용 가능하도록 수정

--- separate.py
+++ separate.py
@@ -75,7 +75,12 @@ class Predictor:
             n_fft=args["n_fft"]
         )
         
-        self.model = ort.InferenceSession(args['model_path'])
+        if torch.cuda.is_available():
+            self.model = ort.InferenceSession(args['model_path'], providers=["CUDAExecutionProvider"])
+        else:
+            self.model = ort.InferenceSession(args['model_path'])
+        
+        #self.model = ort.InferenceSession("/home/jupyter/whisper/denoise_models/UVR-MDX-NET-Inst_Main.onnx")
 
     def demix(self, mix):
         samples = mix.shape[-1]
